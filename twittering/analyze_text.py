85d15c6be87164c4b0e50628e0b990460b889726
RedSquirrelious <redsquirrelious@gmail.com>
2017-01-14 20:16:35 -0800

    cleaning

--- twittering/analyze_text.py
+++ twittering/analyze_text.py
@@ -2,6 +2,7 @@ from nltk import word_tokenize, sent_tokenize
 from nltk.tokenize import TweetTokenizer
 from nltk.corpus import stopwords
 # from nltk.sentiment.util 
+import itertools
 
 from gensim.summarization import summarize
 
@@ -32,11 +33,11 @@ tweet = tokenizer.tokenize(text_array[0])
 
 #summarization practice
 
-with open('mlk_dream_speech.txt', 'r') as f:
-    content = f.read()
-    summary = summarize(content, split=True, word_count=100)
-    for i, sentence in enumerate(summary):
-        print("%d) %s" % (i+1, sentence))
+# with open('mlk_dream_speech.txt', 'r') as f:
+#     content = f.read()
+#     summary = summarize(content, split=True, word_count=100)
+#     for i, sentence in enumerate(summary):
+#         print("%d) %s" % (i+1, sentence))
 
 
 # #processing tweets practice
@@ -50,26 +51,13 @@ def process(text, tokenizer=TweetTokenizer(), stopwords=[]):
 
 
 
-# for tweet in text_array:
-# 	tweet_tokenizer = TweetTokenizer()
+for tweet in text_array:
+	tweet_tokenizer = TweetTokenizer()
 
-# 	tokens = process(tweet, tokenizer=tweet_tokenizer, stopwords=stopword_list)
+	tokens = process(tweet, tokenizer=tweet_tokenizer, stopwords=stopword_list)
 
-# 	tf.update(tokens)
+	tf.update(tokens)
 
-# for tag, count in tf.most_common(20):
-# 		print("{}: {}".format(tag, count))
+for tag, count in tf.most_common(20):
+		print("{}: {}".format(tag, count))
 
-
-
-
-import itertools
-from nltk.collocations import BigramCollocationFinder
-from nltk.metrics import BigramAssocMeasures
- 
-def bigram_word_feats(words, score_fn=BigramAssocMeasures.chi_sq, n=200):
-    bigram_finder = BigramCollocationFinder.from_words(words)
-    bigrams = bigram_finder.nbest(score_fn, n)
-    return dict([(ngram, True) for ngram in itertools.chain(words, bigrams)])
- 
-# evaluate_classifier(bigram_word_feats)
